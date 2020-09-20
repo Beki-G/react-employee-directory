b6e49b874f674052c4e3819e013d8497306f1ff3
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-09-19 21:04:39 -0700

    Updates

---
+++ precache-manifest.127d635f353d8ea0a025c57c37955078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57eb5db32cd29232ca7c3057cd44c0d9",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "07bd4e7af037bec9de22",
+    "url": "/react-employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a99a5eea9dfa815a85fc",
+    "url": "/react-employee-directory/static/js/2.8e1b47c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.8e1b47c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bd4e7af037bec9de22",
+    "url": "/react-employee-directory/static/js/main.0965c9ca.chunk.js"
+  },
+  {
+    "revision": "e82547d4095795d10a4a",
+    "url": "/react-employee-directory/static/js/runtime-main.93471c7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
